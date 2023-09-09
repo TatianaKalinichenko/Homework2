bbb1b9e52d3bd76692c59fa652fba6ec6bb2472b
Tatiana Kalinichenko <tatiankalinichenko@gmail.com>
2023-09-09 20:48:18 +0300

    solved Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number *= -1;
+
+int secondDigit = number / 10 % 10;
+Console.WriteLine($"Вторая цифра числа {number} -> {secondDigit}");
